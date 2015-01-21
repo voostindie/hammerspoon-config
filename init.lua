54f01e82115b5f18a1aeef3d00580a562f7e171b
Vincent Oostindie <vincent@ulso.nl>
2015-01-21 23:49:15 +0100

    Added some basic window management functions
    Mash + C -> Center the frontmost window
    Mash + M -> Maximize the frontmost window (toggle!)
    
    When maximizing a window, the original frame is stored in memory. When
    pressing the hotkey a second time, the window is reset to the original
    frame. Simple, but effective.
    
    The code is kept simple on purpose. If you manually resize a maximized
    window, it will not be maximized. Instead it will be reset to the
    previous dimensions. No problem for me.

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 hs.alert("(Re)loading Hammerspoon configuration", 1)
 
+local cmd = {"cmd"}
+local mash = {"cmd", "alt", "ctrl"}
 --
 -- Toggle application visibility
 --
@@ -13,8 +15,8 @@ function toggleApplication(name)
     end
 end
 
-hs.hotkey.bind({"cmd"}, "§", function()
-    toggleApplication("Terminal")
+hs.hotkey.bind(cmd, "§", function()
+   toggleApplication("Terminal")
 end)
 
 --
@@ -39,24 +41,102 @@ function setCaffeineDisplay(state)
     end
 end
 
-function caffeineClicked()
+function toggleCaffeine()
     setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
 end
 
 if caffeine then
-    caffeine:setClickCallback(caffeineClicked)
+    caffeine:setClickCallback(toggleCaffeine)
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "=", function()
-    caffeineClicked()
+hs.hotkey.bind(mash, "=", function()
+    toggleCaffeine()
+end)
+
+--
+-- Window management
+--
+
+hs.window.animationDuration = 0 -- No animations please
+
+function centerFrontMostWindow()
+    local frontMostWindow = hs.window.focusedWindow()
+    if not frontMostWindow then return end
+    local windowFrame = frontMostWindow:frame()
+    local screenFrame = frontMostWindow:screen():frame()
+    windowFrame.x = (screenFrame.w - windowFrame.w) / 2
+    windowFrame.y = (screenFrame.h - windowFrame.h) / 2
+    frontMostWindow:setFrame(windowFrame)
+end
+
+hs.hotkey.bind(mash, "c", function()
+    centerFrontMostWindow()
+end)
+
+-- When maximizing windows, the old frame is stored in memory so that a
+-- second maximize can reset it. Window sizes are kept in memory and not
+-- persisted (yet)! If there are 50 maximized windows in memory, all old,
+-- no longer existing windows are removed (this hardly ever happens).
+-- When reloading the configuration all maximized windows are first reset,
+-- because otherwise the original settings are lost.
+
+local maximizedWindows = {}
+local maximizedWindowCount = 0
+
+function purgeMaximizedWindowsIfNeeded()
+    if maximizedWindowCount < 50 then return end
+    local purgeCount = 0
+    for id, frame in pairs(maximizedWindows) do
+        local window = hs.window.windowForID(id)
+        if not window then
+            maximizedWindows[id] = nil
+            maximizedWindowCount = maximizedWindowCount - 1
+            purgeCount = purgeCount + 1
+        end
+    end
+end
+
+function maximizeWindow(window)
+    maximizedWindows[window:id()] = window:frame()
+    window:maximize()
+    maximizedWindowCount = maximizedWindowCount + 1
+    purgeMaximizedWindowsIfNeeded()
+end
+
+function restoreWindow(window)
+    local id = window:id()
+    window:setFrame(maximizedWindows[id])
+    maximizedWindows[id] = nil
+    maximizedWindowCount = maximizedWindowCount - 1
+end
+
+function toggleFrontMostWindowMaximized()
+    local frontMostWindow = hs.window.focusedWindow()
+    if not frontMostWindow then return end
+    if maximizedWindows[frontMostWindow:id()] then
+        restoreWindow(frontMostWindow)
+    else
+        maximizeWindow(frontMostWindow)
+    end
+end
+
+function restoreMaximizedWindows()
+    for id, frame in pairs(maximizedWindows) do
+        local window = hs.window.windowForID(id)
+        if window then restoreWindow(window) end
+    end
+end
+
+hs.hotkey.bind(mash, "m", function()
+    toggleFrontMostWindowMaximized()
 end)
 
 --
 -- Date and time
 --
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-", function()
+hs.hotkey.bind(mash, "-", function()
     hs.alert(os.date("It's %R on %B %e, %G"), 2)
 end)
 
@@ -64,8 +144,20 @@ end)
 -- Configuration reloading and alerting
 --
 
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function()
+function reloadConfiguration()
     caffeine:delete()
     caffeine = nil
+    restoreMaximizedWindows()
+    maximizedWindows = nil
+    maximizedWindowCount = nil
     hs.reload()
-end):start()
+end
+
+hs.hotkey.bind(mash, "r", function()
+    reloadConfiguration()
+end)
+
+
+-- hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function()
+--     reloadConfiguration()
+-- end):start()
